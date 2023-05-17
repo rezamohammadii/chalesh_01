4b3d486e9d33bca3902c4e9d73f709ddae3fa6b3
mr-mohammadi <mr-mohammadi@amnpardaz.ir>
2023-05-17 18:08:17 +0430

    add user model

---
+++ chalesh_01/chalesh_01.core/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chalesh_01.core.Models
+{
+    public class UserModel
+    {
+        public int id { get; set; }
+        [Required]
+        [MaxLength(128)]
+        public string FirstName { get; set; } = null!;
+        [MaxLength(128)]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        [MaxLength(128)]
+        public int Age { get; set; }  // The max length(128) for the "age" field is not logical : logical length 3       
+        public string Website { get; set; } = string.Empty;
+    }
+}
