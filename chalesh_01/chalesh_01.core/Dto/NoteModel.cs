268ae962b9e5b83614a05b868dd477f77aa51144
mr-mohammadi <mr-mohammadi@amnpardaz.ir>
2023-05-17 20:27:58 +0430

    add note class

---
+++ chalesh_01/chalesh_01.core/Dto/NoteModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chalesh_01.core.Dto
+{
+    public class NoteModel
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ chalesh_01/chalesh_01.core/Dto/UserModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chalesh_01.core.Dto
+{
+    public class UserModelIn
+    {
+        [Required]
+        [MaxLength(128)]
+        public string FirstName { get; set; } = null!;
+        [MaxLength(128)]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public int Age { get; set; }  // The max length(128) for the "age" field is not logical        
+        public string Website { get; set; } = string.Empty;
+    }
+
+    public class UserModelOut
+    {
+        public int id { get; set; } = RandomNumber.RandomId();
+        [Required]
+        [MaxLength(128)]
+        public string FirstName { get; set; } = null!;
+        [MaxLength(128)]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        public int Age { get; set; }  // The max length(128) for the "age" field is not logical        
+        public string Website { get; set; } = string.Empty;
+    }
+
+    public class RandomNumber
+    {
+        public static int RandomId()
+        {
+            Random random = new Random();
+            return random.Next(10, 100);
+        }
+       
+    }
+
+}
