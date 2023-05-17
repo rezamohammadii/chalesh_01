45cb576b19fbc3635ad84c8a4a505f077977299d
mr-mohammadi <mr-mohammadi@amnpardaz.ir>
2023-05-17 20:27:44 +0430

    add mapper class

---
+++ chalesh_01/chalesh_01.core/Dto/DataContextProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chalesh_01.core.Dto
+{
+    public class DataContextProfile : Profile
+    {
+        public DataContextProfile()
+        {
+            CreateMap<UserModelIn, UserModelOut>()
+            .ReverseMap();
+        }
+        
+    }
+}
