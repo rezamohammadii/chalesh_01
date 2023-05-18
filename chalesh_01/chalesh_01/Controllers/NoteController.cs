9433c067ca02a6d00f5ddb0f5b5a884b4719dfc7
mr-mohammadi <mr-mohammadi@amnpardaz.ir>
2023-05-18 14:11:41 +0430

    add CRUD api for notes

---
+++ chalesh_01/chalesh_01/Controllers/NoteController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using chalesh_01.core.CodeFactory;
+using chalesh_01.core.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace chalesh_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        public NoteController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        // GET api/<NoteController>/5
+        [HttpGet("/GetAllNoteUser/{userId}")]
+        public IActionResult GetAllNoteUser(int userId)
+        {
+            var getAllNote = Utils.Notes.Where(n=>n.UserId == userId).ToList();
+            if(getAllNote.Count() == 0) return Ok("No data for userId");
+            return Ok(getAllNote);
+        }
+        [HttpGet("GetNote/{noteId}")]
+        public IActionResult GetNote(int noteId)
+        {
+            if(noteId == 0) return BadRequest();
+            var getNote = Utils.Notes.Where(n => n.Id == noteId).SingleOrDefault();
+            if (getNote == null) return BadRequest("There are no notes with this ID");
+            return Ok(getNote);
+        }
+
+        // POST api/<NoteController>
+        [HttpPost]
+        public IActionResult Post([FromBody] NoteIn note)
+        {
+            if(note == null) return BadRequest();
+            var noteMap = _mapper.Map<Note>(note);
+            Utils.Notes.Add(noteMap);
+            return Ok("Add note");
+        }
+
+        // PUT api/<NoteController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] NoteIn noteIn)
+        {
+            var note = Utils.Notes.Where(n=>n.Id == id).SingleOrDefault();
+            if(note == null) return BadRequest("There are no notes with this ID");
+            note.Name = noteIn.Name;
+            note.Published = noteIn.Published;
+            note.View =noteIn.View;
+            note.DateModified = DateTime.Now;
+            return Ok("The note is updated");
+        }
+
+        // DELETE api/<NoteController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if(id == 0) return BadRequest();
+            var note = Utils.Notes.Where(n=>n.Id == id).SingleOrDefault();
+            if (note == null) return BadRequest("There are no notes with this ID");
+            Utils.Notes.Remove(note);
+            return NoContent();
+
+        }
+    }
+}
--- chalesh_01/chalesh_01/Controllers/UserController.cs
+++ chalesh_01/chalesh_01/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using chalesh_01.core.CodeFactory;
 using chalesh_01.core.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,7 +11,6 @@ namespace chalesh_01.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private static List<UserModelOut> UserList = new List<UserModelOut>();
         private readonly IMapper _mapper;
         public UserController(IMapper mapper)
         {
@@ -20,28 +20,30 @@ namespace chalesh_01.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            if(UserList == null) return StatusCode(204);
-            return Ok(new { result = UserList });
+            
+            if(Utils.UserList == null) return StatusCode(204);
+            return Ok(new { result = Utils.UserList });
         }
 
         // GET api/<UserController>/5
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            if (UserList == null) return StatusCode(204);
-            var user = UserList.Where(u=>u.Age == id).SingleOrDefault();
+            if (Utils.UserList == null) return StatusCode(204);
+            var user = Utils.UserList.Where(u=>u.id == id).SingleOrDefault();
+            var notes = Utils.Notes.Where(x=>x.UserId == id).ToList();
             if (user == null) return Ok(new { result = "User with this ID was not found" });
-            return Ok(new { result = user });
+            return Ok(new { result = user, notes });
         }
 
         // POST api/<UserController>
         [HttpPost]
         public IActionResult Post([FromBody] UserModelIn model)
         {
-            var checkDuplicateEmail = UserList.Where(u=>u.Email == model.Email).Any();
+            var checkDuplicateEmail = Utils.UserList.Where(u=>u.Email == model.Email).Any();
             if (checkDuplicateEmail) return BadRequest(new { message = "There is a user with this email" });
             var user = _mapper.Map<UserModelOut>(model);                
-            UserList.Add(user);
+            Utils.UserList.Add(user);
             return Ok(new { result = user });
         }
 
@@ -49,7 +51,7 @@ namespace chalesh_01.Controllers
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] UserModelIn model)
         {
-            var user = UserList.Where(x=>x.id == id).SingleOrDefault();
+            var user = Utils.UserList.Where(x=>x.id == id).SingleOrDefault();
             if (user == null) return BadRequest(new { message = "User is not exsit" });            
             user.LastName = model.LastName;
             user.FirstName = model.FirstName;
@@ -61,11 +63,16 @@ namespace chalesh_01.Controllers
 
         // DELETE api/<UserController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
-            var user = UserList.Where(x => x.id == id).SingleOrDefault();
+            var user = Utils.UserList.Where(x => x.id == id).SingleOrDefault();
             if (user == null) return BadRequest(new { message = "User is not exsit" });
-            UserList.Remove(user);
+            Utils.UserList.Remove(user);
+            var nots = Utils.Notes.Where(x => x.UserId == id).ToList();
+            foreach (var item in nots)
+            {
+                Utils.Notes.Remove(item);
+            }
             return NoContent();
         }
     }
