deabe5ccb207d3ca564e9d3e97cd0f252606f887
mr-mohammadi <mr-mohammadi@amnpardaz.ir>
2023-05-18 12:31:14 +0430

    HasProperty function

---
+++ chalesh_01/chalesh_01.core/CodeFactory/Utils.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chalesh_01.core.CodeFactory
+{
+    public class Utils
+    {
+        public static bool HasProperty(JObject obj, string propertyName)
+        {
+            var checkKey = obj.ContainsKey(propertyName);
+            if (!checkKey) return false;
+            return true;
+        }
+
+        public static string? GetPropertyValue(JObject obj, string propertyName)
+        {
+            if(string.IsNullOrEmpty(propertyName)) return null;
+            obj.TryGetValue(propertyName, out var value);   
+            if (value == null) return null;
+            return value.ToString();
+        }
+    }
+}
